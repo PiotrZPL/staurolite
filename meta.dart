be9f44cb8a42e869f56a8aa7b78b7e6c6ed852a6
PiotrZPL <zpl20052020@protonmail.ch>
2022-11-20 11:22:20 +0100

    Added meta tag

---
+++ meta.dart
@@ -0,0 +1,28 @@
+import 'htmlwidget.dart';
+
+class Meta extends HtmlWidget {
+  Meta({this.charset, this.content, this.http_equiv, this.name});
+
+  final String? charset;
+  final String? content;
+  final String? http_equiv;
+  final String? name;
+
+  @override
+  String toHTML() {
+    String output = "<meta ";
+    if (charset != null) {
+      output += """charset="$charset" """;
+    }
+    if (name != null) {
+      output += """name="$name" """;
+    }
+    if (http_equiv != null) {
+      output += """http-equiv="$http_equiv" """;
+    }
+    if (content != null) {
+      output += """content="$content" """;
+    }
+    return output.trim() + ">";
+  }
+}
