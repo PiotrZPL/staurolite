9da7438f599bb5bd8deb3f655bce89473c85a133
Piotr Lange <zpl20052020@protonmail.ch>
2022-12-29 08:23:23 +0100

    Added support for custom config files

--- lib/src/buildWebsite.dart
+++ lib/src/buildWebsite.dart
@@ -2,7 +2,7 @@ import 'website.dart';
 import 'dart:io';
 import 'package:serve/serve.dart';
 
-void buildWebsite(Website website) async {
+void buildWebsite(Website website, [String customConfigPath = ""]) async {
   String buildDir = "build";
   String outputDir = "$buildDir/output";
   stdout.write("Checking if $buildDir exists... ");
@@ -41,116 +41,122 @@ void buildWebsite(Website website) async {
   await newStyle.writeAsString("""@tailwind base;
 @tailwind components;
 @tailwind utilities;""");
-  final newConfig = await File("$buildDir/tailwind.config.js").create(recursive: true);
-  stdout.write("Creating TailwindCSS config file...\n");
-  await newConfig.writeAsString("""/** @type {import('tailwindcss').Config} */
-
-const colors = require('tailwindcss/colors')
-
-module.exports = {
-  content: ["./**/*.html"],
-  plugins: [],darkMode: "class",
-  theme: {
-    extend: {
-      colors: {
-        amber: colors.amber,
-        black: colors.black,
-        blue: colors.blue,
-        blueGray: colors.blueGray,
-        coolGray: colors.coolGray,
-        cyan: colors.cyan,
-        emerald: colors.emerald,
-        fuchsia: colors.fuchsia,
-        // gray: colors.gray,
-        green: colors.green,
-        indigo: colors.indigo,
-        lightBlue: colors.lightBlue,
-        lime: colors.lime,
-        orange: colors.orange,
-        pink: colors.pink,
-        purple: colors.purple,
-        red: colors.red,
-        rose: colors.rose,
-        sky: colors.sky,
-        teal: colors.teal,
-        trueGray: colors.trueGray,
-        violet: colors.violet,
-        warmGray: colors.warmGray,
-        white: colors.white,
-        yellow: colors.yellow,
-      },
-      typography: (theme) => ({
-        dark: {
-          css: [
-            {
-              color: theme("colors.gray.400"),
-              '[class~="lead"]': {
-                color: theme("colors.gray.300"),
-              },
-              a: {
-                color: theme("colors.white"),
-              },
-              strong: {
-                color: theme("colors.white"),
-              },
-              "ol > li::before": {
-                color: theme("colors.gray.400"),
-              },
-              "ul > li::before": {
-                backgroundColor: theme("colors.gray.600"),
-              },
-              hr: {
-                borderColor: theme("colors.gray.200"),
-              },
-              blockquote: {
-                color: theme("colors.gray.200"),
-                borderLeftColor: theme("colors.gray.600"),
-              },
-              h1: {
-                color: theme("colors.white"),
-              },
-              h2: {
-                color: theme("colors.white"),
-              },
-              h3: {
-                color: theme("colors.white"),
-              },
-              h4: {
-                color: theme("colors.white"),
-              },
-              "figure figcaption": {
-                color: theme("colors.gray.400"),
-              },
-              code: {
-                color: theme("colors.white"),
-              },
-              "a code": {
-                color: theme("colors.white"),
-              },
-              pre: {
-                color: theme("colors.gray.200"),
-                backgroundColor: theme("colors.gray.800"),
-              },
-              thead: {
-                color: theme("colors.white"),
-                borderBottomColor: theme("colors.gray.400"),
-              },
-              "tbody tr": {
-                borderBottomColor: theme("colors.gray.600"),
-              },
-            },
-          ],
+  if (customConfigPath != "") {
+    final newConfig = await File("$buildDir/tailwind.config.js").create(recursive: true);
+    stdout.write("Creating TailwindCSS config file...\n");
+    await newConfig.writeAsString("""/** @type {import('tailwindcss').Config} */
+    
+    const colors = require('tailwindcss/colors')
+    
+    module.exports = {
+    content: ["./**/*.html"],
+    plugins: [],darkMode: "class",
+    theme: {
+      extend: {
+        colors: {
+          amber: colors.amber,
+          black: colors.black,
+          blue: colors.blue,
+          blueGray: colors.blueGray,
+          coolGray: colors.coolGray,
+          cyan: colors.cyan,
+          emerald: colors.emerald,
+          fuchsia: colors.fuchsia,
+          // gray: colors.gray,
+          green: colors.green,
+          indigo: colors.indigo,
+          lightBlue: colors.lightBlue,
+          lime: colors.lime,
+          orange: colors.orange,
+          pink: colors.pink,
+          purple: colors.purple,
+          red: colors.red,
+          rose: colors.rose,
+          sky: colors.sky,
+          teal: colors.teal,
+          trueGray: colors.trueGray,
+          violet: colors.violet,
+          warmGray: colors.warmGray,
+          white: colors.white,
+          yellow: colors.yellow,
         },
-      }),
+        typography: (theme) => ({
+          dark: {
+            css: [
+              {
+                color: theme("colors.gray.400"),
+                '[class~="lead"]': {
+                  color: theme("colors.gray.300"),
+                },
+                a: {
+                  color: theme("colors.white"),
+                },
+                strong: {
+                  color: theme("colors.white"),
+                },
+                "ol > li::before": {
+                  color: theme("colors.gray.400"),
+                },
+                "ul > li::before": {
+                  backgroundColor: theme("colors.gray.600"),
+                },
+                hr: {
+                  borderColor: theme("colors.gray.200"),
+                },
+                blockquote: {
+                  color: theme("colors.gray.200"),
+                  borderLeftColor: theme("colors.gray.600"),
+                },
+                h1: {
+                  color: theme("colors.white"),
+                },
+                h2: {
+                  color: theme("colors.white"),
+                },
+                h3: {
+                  color: theme("colors.white"),
+                },
+                h4: {
+                  color: theme("colors.white"),
+                },
+                "figure figcaption": {
+                  color: theme("colors.gray.400"),
+                },
+                code: {
+                  color: theme("colors.white"),
+                },
+                "a code": {
+                  color: theme("colors.white"),
+                },
+                pre: {
+                  color: theme("colors.gray.200"),
+                  backgroundColor: theme("colors.gray.800"),
+                },
+                thead: {
+                  color: theme("colors.white"),
+                  borderBottomColor: theme("colors.gray.400"),
+                },
+                "tbody tr": {
+                  borderBottomColor: theme("colors.gray.600"),
+                },
+              },
+            ],
+          },
+        }),
+      },
     },
-  },
-  variants: {
-    extend: {
-      typography: ["dark"],
+    variants: {
+      extend: {
+        typography: ["dark"],
+      },
     },
-  },
-  plugins: [require("@tailwindcss/typography")],
-}""");
+    plugins: [require("@tailwindcss/typography")],
+    }""");
+  }
+  else {
+    stdout.write("Copying custom config $customConfigPath...\n");
+    await File(customConfigPath).copy("$buildDir/tailwind.config.js");
+  }
   stdout.write("Copying files from the static directory\n");
   if (await Directory("static").exists()) {
     final staticDir = Directory("static");
