8e1eabea20a6fd88ef08f6d9201402a57657eb4c
Piotr Lange <zpl20052020@protonmail.ch>
2022-12-30 10:03:35 +0100

    Added window event attributes to HtmlWidget

--- lib/src/htmlbasiccomponents/htmlwidget.dart
+++ lib/src/htmlbasiccomponents/htmlwidget.dart
@@ -3,13 +3,47 @@ class HtmlWidget {
     this.widget_class,
     this.style,
     this.id,
-    this.title
+    this.title,
+
+    // Window event attributes
+    this.onafterprint,
+    this.onbeforeprint,
+    this.onbeforeunload,
+    this.onerror,
+    this.onhashchange,
+    this.onload,
+    this.onmessage,
+    this.onoffline,
+    this.ononline,
+    this.onpagehide,
+    this.onpageshow,
+    this.onpopstate,
+    this.onresize,
+    this.onstorage,
+    this.onunload
   });
 
   final String? widget_class;
   final String? style;
   final String? id;
   final String? title;
+
+  // Window event attributes
+  final String? onafterprint;
+  final String? onbeforeprint;
+  final String? onbeforeunload;
+  final String? onerror;
+  final String? onhashchange;
+  final String? onload;
+  final String? onmessage;
+  final String? onoffline;
+  final String? ononline;
+  final String? onpagehide;
+  final String? onpageshow;
+  final String? onpopstate;
+  final String? onresize;
+  final String? onstorage;
+  final String? onunload;
 
   String toHTML() {
     return "OK";
