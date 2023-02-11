ba776394c86c69c86ed68c9adae84ba610c964b4
PiotrZPL <zpl20052020@protonmail.ch>
2023-02-11 14:09:30 +0100

    Renamed widget_class to properties

--- lib/src/htmlbasiccomponents/header.dart
+++ lib/src/htmlbasiccomponents/header.dart
@@ -2,18 +2,18 @@ import 'htmlwidget.dart';
 
 class Header extends HtmlWidget{
   Header({
-    widget_class,
+    properties,
     style,
     id,
     this.widgets
-  }) : super(widget_class: widget_class, style: style, id: id);
+  }) : super(properties: properties, style: style, id: id);
 
   final List<HtmlWidget?>? widgets;
 
   String toHTML() {
     var output = "<header ";
-    if (widget_class != null) {
-      output += """class="$widget_class" """;
+    if (properties != null) {
+      output += """class="$properties" """;
     }
     output = output.trim() + ">";
     if (widgets != null) {
