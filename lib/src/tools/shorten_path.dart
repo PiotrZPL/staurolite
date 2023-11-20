832b795756aabc208ba02b05fe9dfe9b1e24fc2b
PiotrZPL <zpl20052020@protonmail.ch>
2023-11-20 19:17:31 +0100

    Preserve "/" at the end of a path

--- lib/src/tools/shorten_path.dart
+++ lib/src/tools/shorten_path.dart
@@ -1,6 +1,6 @@
 String shortenStandardPath(String path) {
   if (path.endsWith("/index.html")) {
-    return path.substring(0, path.length - 11);
+    return path.substring(0, path.length - 10);
   }
   else {
     return path;
